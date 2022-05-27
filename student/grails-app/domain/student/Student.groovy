f2cb6d9d2d15dc97c7dc587a3372de3caf941b17
rick pascua <rick.pascua@gmail.com>
2022-05-27 15:11:14 +0800

    'updates'

--- student/grails-app/domain/student/Student.groovy
+++ student/grails-app/domain/student/Student.groovy
@@ -1,7 +1,25 @@
 package student
 
+import grails.rest.*
+
+
+@Resource(uri='/api/student', formats=['json'])
 class Student {
 
+    Long id
+    String name
+    String email
+    String phone
+
     static constraints = {
+        name size: 5..40, blank: false, unique: true
+        email size: 5..30, blank: false
+        phone size: 5..15, blank: false
+    }
+
+    static mapping = {
+        version false
+        table 'student'
+        id column:'id', generator: 'native', params:[sequence:'student_seq']
     }
 }
